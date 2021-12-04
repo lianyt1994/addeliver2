40f1af4fd585ca383176f30958678f02e03a8bce
lianting@zju.edu.cn <lianting@zju.edu.cn>
2021-12-04 20:25:07 +0800

    Initial commit

---
+++ src/main/java/com/imooc/addeliver2/Addeliver2Application.java
@@ -0,0 +1,13 @@
+package com.imooc.addeliver2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Addeliver2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Addeliver2Application.class, args);
+	}
+
+}
